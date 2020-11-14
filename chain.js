13003c99aa57b128663ee5ab5637458363823b86
papakipari555 <72609151+papakipari555@users.noreply.github.com>
2020-11-14 16:57:06 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain{
+constructor(bodyA , bodyB){
+    var options = {
+        bodyA : bodyA,
+        bodyB : bodyB,
+        length : 50,
+        stiffness : 0.8
+    }
+    this.chain= Matter.Constraint.create(options)
+    World.add(world,this.chain)
+}
+display(){
+    line(this.chain.bodyA.position.x , this.chain.bodyA.position.y ,this.chain.bodyB.position.x , this.chain.bodyB.position.y  )
+}
+
+}
--- sketch.js
+++ sketch.js
@@ -33,9 +33,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(100,100,150 , PI/2);
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body , log6.body )
 }
 
 function draw(){
@@ -58,7 +59,11 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
+    
     bird.display();
+
+    chain.display();
+    
     platform.display();
 }
